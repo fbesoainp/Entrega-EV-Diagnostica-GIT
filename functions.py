104b1210a3808c8e37f985c5358cad765600e798
fbesoainp <53845524+fbesoainp@users.noreply.github.com>
2022-03-28 20:04:35 -0300

    feat: topUsers function

--- functions.py
+++ functions.py
@@ -1,15 +1,26 @@
 import operator
+import re
 
 def topTweets(data):
     data.sort(key=operator.itemgetter('retweetCount'), reverse=True)
     return data[:10]
 
-
 def topUsers(data):
-    pass
+    users = []
+    ids = []
+
+    for tweet in data:
+        if tweet['user']['id'] not in ids:
+            users.append(tweet['user'])
+            ids.append(tweet['user']['id'])
+
+    users.sort(key=operator.itemgetter('statusesCount'), reverse=True)
+    return users[:10]
 
 def topDays(data):
-    pass 
+    pass
+
+
 
 def topHashtags(data):
     pass
