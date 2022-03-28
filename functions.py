c294cbdcb8a6cb94450e91378bf0f1b72f8e9c9a
fbesoainp <53845524+fbesoainp@users.noreply.github.com>
2022-03-28 19:13:39 -0300

    feat: main function

---
+++ functions.py
@@ -0,0 +1,11 @@
+def topTweets(data):
+    pass
+
+def topUsers(data):
+    pass
+
+def topDays(data):
+    pass 
+
+def topHashtags(data):
+    pass
---
+++ main.py
@@ -0,0 +1,28 @@
+import json
+import functions
+
+data = [json.loads(line) for line in open('data/farmers-protest-tweets-2021-03-5.json', 'r')]
+
+if __name__ == "__main__":
+    while True:
+        msg = """Seleccione funcion a ejecutar:\n
+        1. Los top 10 tweets más retweeted\n
+        2. Los top 10 usuarios en función a la cantidad de tweets que emitieron\n
+        3. Los top 10 días donde hay más tweets\n
+        4. Top 10 hashtags más usados\n
+        5. Cerrar\n"""
+        usr = input(msg)
+
+        if usr != "5":
+            if usr == "1":
+                functions.topTweets(data)
+            elif usr == "2":
+                functions.topUsers(data)
+            elif usr == "3":
+                functions.topDays(data)
+            elif usr == "4":
+                functions.topHashtags(data)
+            else:
+                print("Input invalido\n")
+        else:
+            break
